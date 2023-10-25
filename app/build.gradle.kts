0600749cc019f4486dc0065b42e0c3f9fef39277
qcx330 <quynhpham3702@outlook.com>
2023-10-25 23:30:12 +0700

    layout singin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.familychat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.familychat"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
